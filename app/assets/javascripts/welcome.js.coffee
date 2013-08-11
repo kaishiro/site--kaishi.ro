1319d52a2125018fe66c3c0aa637864e42149386
Matthew White <matt@substructu.re>
2013-08-11 19:28:45 -0400

    Adding resources for welcome index view

--- app/assets/javascripts/welcome.js.coffee
+++ app/assets/javascripts/welcome.js.coffee
@@ -1,3 +1,6 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
+jQuery ->
+  $(".hello__greeting").fitText(.4);
+  $(".hello__name").fitText(.4);
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,27 @@
 class WelcomeController < ApplicationController
   def index
+    @today    = Time.now.in_time_zone('Eastern Time (US & Canada)').midnight.utc
+    @tomorrow = @today + 1.day
+    
+    @sleeps = Sleep.all
+    @exercises = Exercise.all
+    @vitamins = Vitamin.all
+    @waters = Water.all
+    @expenses = Expense.all
+  
+#   weights = Weight.where(:created_at => Time.now.beginning_of_day..Time.now.end_of_day)
+    @weight = Weight.select("weight").where(:created_at => @today..@tomorrow).average("weight")
+ 
+    @foods = Food.where(:created_at => @today..@tomorrow)
+
+    @exercises = Exercise.where(:created_at => @today..@tomorrow)
+
+    @vitamins = Vitamin.where(:created_at => @today..@tomorrow)
+
+    @waters = Water.where(:created_at => @today..@tomorrow)
+
+    @sleep = Sleep.order("created_at").last
+ 
   end
+
 end
