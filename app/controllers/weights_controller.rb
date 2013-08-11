13114409ff80bcba52ec7781b7091f12634d0102
Matthew White <matt@substructu.re>
2013-08-10 23:00:56 -0400

    Adding weights model and controller

---
+++ app/controllers/weights_controller.rb
@@ -0,0 +1,21 @@
+class WeightsController < ApplicationController
+
+  def index
+    @weights = Weight.all
+  end
+
+  def create
+
+    @weight = Weight.new(post_params)
+
+    @weight.save
+    redirect_to @weight
+
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:body)
+    end
+
+end
